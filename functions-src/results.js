300ea48d2e7cc74d22ab9ce6cab8badd60f7c1c4
Eduardo Bouças <mail@eduardoboucas.com>
2018-12-18 23:53:17 +0000

    refactor: reorganise packages

---
+++ functions-src/results.js
@@ -0,0 +1,9 @@
+const SpeedTracker = require('@speedtracker/api')
+const MongoDB = require('@speedtracker/mongodb')
+const database = new MongoDB({
+  connectionString: process.env.MONGODB_CONN_STR,
+  database: process.env.MONGODB_DATABASE
+})
+const api = new SpeedTracker(database)
+
+module.exports.handler = api.getResultsFunction()
